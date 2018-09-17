d2c5a00b2ae86e15a62457567cc64aa92ee3a5d0
Shannan Galbraith <s.galbraith08@gmail.com>
2018-09-18 06:15:12 +1000

    tidy up log model

--- models/log.js
+++ models/log.js
@@ -23,10 +23,7 @@ module.exports = (sequelize, DataTypes) => {
             msg: `route does not exist`
           }
         }
-      },
-      threadId: DataTypes.INTEGER,
-      userId: DataTypes.INTEGER,
-      sessionUserId: DataTypes.INTEGER
+      }
     },
     {}
   );
