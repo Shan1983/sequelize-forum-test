8f3b39426511952efc38c5f3eecd052c453c429e
Shannan Galbraith <s.galbraith08@gmail.com>
2018-09-22 10:52:07 +1000

    completed log route - requires testing

--- routes/log.js
+++ routes/log.js
@@ -112,4 +112,154 @@ router.post("/", async (req, res, next) => {
   }
 });
 
+// router.all('*', (req, res, next) => {
+//     if(req.session.admin) {
+//         next()
+//     }else {
+//         res.status(401)
+//         res.json({
+//             errors: [Errors.requestNotAuthorized]
+//         })
+//     }
+// })
+
+router.get("/top-threads", async (req, res, next) => {
+  try {
+    const logs = await Log.findAll({
+      where: {
+        createdAt: {
+          $gt: new Date(Date.now() - 1000 * 60 * 60 * 24)
+        },
+        route: "thread"
+      },
+      include: [Thread]
+    });
+
+    // sum each log for a thread
+    const pageViewsObj = logs.reduce((obj, log) => {
+      // E.g. if thread deleted
+      if (!log.Thread) {
+        return obj;
+      }
+
+      if (!obj[log.Thread.id]) {
+        obj[log.Thread.id] = {
+          Thread: log.Thread,
+          pageViews: 1
+        };
+      } else {
+        obj[log.Thread.id].pageViews++;
+      }
+
+      return obj;
+    }, {});
+
+    // transform to array
+    const pageViewsArr = Object.keys(pageViewsObj).maps(id => {
+      return pageViewsObj[id];
+    });
+
+    // sort by number of page views decending
+    const sortedPageViewsArr = pageViewsArr.sort((a, b) => {
+      if (a.pageViews < b.pageViews) {
+        return 1;
+      } else if (a.pageViews > b.pageViews) {
+        return -1;
+      } else {
+        return 0;
+      }
+    });
+
+    // return top 3
+    res.json(sortedPageViewsArr.slice(0, 4));
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/page-views", async (req, res, next) => {
+  try {
+    const users = await Log.findAll({
+      where: {
+        createdAt: {
+          $gt: lastWeek
+        }
+      },
+      order: [["createdAt", "ASC"]]
+    });
+
+    res.json(processLogsForLineChart(logs));
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/new-users", async (req, res, next) => {
+  try {
+    const users = await User.findAll({
+      where: {
+        createdAt: {
+          $gt: lastWeek
+        }
+      },
+      order: [["createdAt", "ASC"]]
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/categories", async (req, res, next) => {
+  try {
+    const categories = await Category.findAll();
+    const categoryThreadCount = [];
+
+    await Promise.all(
+      categories.map(async category => {
+        const count = await Thread.count({
+          where: { CategoryId: category.id }
+        });
+        categoryThreadCount.push({
+          value: count,
+          label: category.name,
+          color: category.color
+        });
+      })
+    );
+
+    res.json(categoryThreadCount);
+  } catch (e) {
+    next(e);
+  }
+});
+
+router.get("/new-thread", async (req, res, next) => {
+  try {
+    const now = Date.now();
+    const threadsTodayCount = await Thread.count({
+      where: {
+        createdAt: {
+          $gt: new Date(now - 1000 * 60 * 60 * 24)
+        }
+      }
+    });
+
+    const threadsTesterdayCount = await Thread.count({
+      where: {
+        createdAt: {
+          $lt: new Date(now - 1000 * 60 * 60 * 24),
+          $ge: new Date(now - 1000 * 60 * 60 * 24 * 2)
+        }
+      }
+    });
+
+    res.json({
+      count: threadsTesterdayCount,
+      change: threadsTodayCount - threadsTesterdayCount
+    });
+  } catch (e) {
+    next(e);
+  }
+});
+
 module.exports = router;
